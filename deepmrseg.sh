bb244b113814616486bee2c7c1343da2e4e3e9c5
Gilberstadt <gilberstadt.j@login01.cluster>
2022-01-06 14:47:30 -0600

    Script that runs deepmrseg

---
+++ deepmrseg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######## Job Name: Sample_Job ########
+#SBATCH -J Sample_Job
+######## Job Output File: Sample_job.oJOBID ########
+#SBATCH -o /home/gilberstadt.j/Upload_from_Local/deepmrseg_jobs/Sample_job.o%j
+######## Job Error File: Sample_job.eJOBID ########
+#SBATCH -e /home/gilberstadt.j/Upload_from_Local/deepmrseg_jobs/Sample_job.e%j
+######## Email
+#SBATCH --mail-user=gilberstadt.j@wustl.edu
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+######## Number of tasks: 1 ########
+#SBATCH -n 1
+######## Memory per node: 200 MB ########
+#SBATCH --mem 25G
+######## Walltime: 30 minutes ########
+#SBATCH -t 5:00:00
+#SBATCH --gres gpu:1,vmem:32gb:1
+
+module load python
+source activate DeepMRSeg
+
+fullpath=$1
+dirpath=$(dirname ${fullpath}) # gives you the path to the directory where the image lives
+fname=$(basename ${fullpath}) # gives you the name of the image file
+fbase=$(echo ${fname} | rev | cut -f3- -d '.' | rev)
+deepmrseg_apply --task dlicv --inImg ${fullpath} --outImg /scratch/gilberstadt.j/ADNI_data/final_DeepMRSeg_GPU/${fbase}_mask.nii.gz
