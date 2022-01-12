c25d3e5d672018d57d6f78bdea498a084d4873f5
Gilberstadt <gilberstadt.j@login02.cluster>
2022-01-12 17:37:37 -0600

    Script that runs HD-BET using cpu

---
+++ hd-bet_cpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+######## Job Name: Sample_Job ########
+#SBATCH -J Sample_Job
+######## Job Output File: Sample_job.oJOBID ########
+#SBATCH -o /home/gilberstadt.j/Upload_from_Local/hd-bet_jobs/Sample_job.o%j
+######## Job Error File: Sample_job.eJOBID ########
+#SBATCH -e /home/gilberstadt.j/Upload_from_Local/hd-bet_jobs/Sample_job.e%j
+######## Email
+#SBATCH --mail-user=gilberstadt.j@wustl.edu
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+######## Number of tasks: 1 ########
+#SBATCH -n 1
+######## Memory per node: 200 MB ########
+########SBATCH --mem 5G
+######## Walltime: 30 minutes ########
+#SBATCH -t 50:00:00
+
+module load python
+
+fullpath=$1  # the full path to the image
+dirpath=$(dirname ${fullpath}) # gives you the path to the directory where the image lives
+fname=$(basename ${fullpath}) # gives you the name of the image file
+fbase=$(echo ${fname} | rev | cut -f3- -d '.' | rev)
+
+hd-bet -i ${fullpath} -o /scratch/gilberstadt.j/ADNI_data/hd-bet_output_test_bids/${fname}
